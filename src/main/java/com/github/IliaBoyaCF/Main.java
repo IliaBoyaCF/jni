ef4ec5b1225fdb3ba433ea8b62c36983eb141691
IliaBoyaCF <ilia.boya.cf@yandex.ru>
2025-02-17 16:19:32 +0700

    Remove redunant feature.

--- src/main/java/com/github/IliaBoyaCF/Main.java
+++ src/main/java/com/github/IliaBoyaCF/Main.java
@@ -1,11 +1,8 @@
 package com.github.IliaBoyaCF;
 
-import com.github.IliaBoyaCF.integral.ApproximateIntegral;
-import com.github.IliaBoyaCF.integral.Initials;
-
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println(ApproximateIntegral.calculate(ApproximateIntegral.StandardMethods.SIMPSONS_RULE, new Initials(Math::log, 1, 10, 100)));
+        System.out.println("Java numeric integration utility.");
     }
 }
